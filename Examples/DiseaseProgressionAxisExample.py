Fixed a numerical problem in the forcePos monotonicPositive function. This commit is to save the normalization introduced in the tf_keras_tfp_lbfg.py file, which will be removed, since the normalization is now moved to the variable definition.

Examples/DiseaseProgressionAxisExample.py
@@ -9,19 +9,25 @@ ProgressionTime = 20
 M.addAxis("Disease Progression", ProgressionTime, queue=True)
 
 I0 = M.newVariables({"I0": 1.0})  # initially infected
-InitProgression = lambda: I0 * M.Axes['Disease Progression'].initDelta()  # variables to fit have to always be packed in lambda functions!
-M.newState(name='progression', axesInit={'Disease Progression': InitProgression})  # initially someone infected
+M.newState(name='susceptible', axesInit=1.0)  # initially someone infected
+# InitProgression = lambda: I0() * M.Axes['Disease Progression'].initDelta()  # variables to fit have to always be packed in lambda functions!
+# M.newState(name='progression', axesInit={'Disease Progression': InitProgression})  # initially someone infected
+M.newState(name='progression', axesInit={'Disease Progression': 0.0})  # initially someone infected
 M.newState(name='hospitalized', axesInit={'Disease Progression': 0.0})  # just the total in hospital
 M.newState(name='recovered', axesInit=0.0)  # the ones that made it through the time series
-toHospitalRate = M.newVariables({"toHospitalTime": 7.0, "toHospitalSigma": 3.0, "toHospitalRate": 0.1})  # average time when sent to hospital, sigma and rate
+toHospitalRate = M.newVariables({"toHospitalTime": 7.0, "toHospitalSigma": 3.0, "toHospitalRate": 0.1}, forcePos=False)  # average time when sent to hospital, sigma and rate
+T0 = M.newVariables({"T0": 10.5}, forcePos=False)  # time at which a delta is injected into the progression
+M.addRate('susceptible', 'progression', lambda t: M.initGaussianT0(T0, t), queueDst='Disease Progression', hasTime=True)  # When you made it though the queue, you are recovered
 hospitalization = lambda: toHospitalRate * M.Axes['Disease Progression'].initGaussian(M.Var['toHospitalTime'],
-                                                                                      M.Var['toHospitalSigma'])
+                                                                                        M.Var['toHospitalSigma'])
 M.addRate('progression', 'hospitalized', hospitalization)  # susc*infec --> infec second order rate
 M.addRate('hospitalized', 'recovered', 1.0, queueSrc='Disease Progression')  # When you made it though the queue, you are recovered
 M.addRate('hospitalized', 'hospitalized', 0.05, queueSrc='total', queueDst='Disease Progression')  # add some chance to start over
 Popul = 100000.0
-M.toFit(['I0', 'toHospitalTime', 'toHospitalRate'])  # 'toHospitalTime',  fit the infectin rate and the initial situation
-to_distort = {'toHospitalTime': 1.3, 'I0': 1.2, 'toHospitalRate': 0.5}
+#M.toFit(['T0', 'toHospitalTime', 'toHospitalRate'])  # 'toHospitalTime',  fit the infection rate and the initial situation
+M.toFit(['T0'])  # 'toHospitalTime',  fit the infection rate and the initial situation
+# to_distort = {'toHospitalTime': 1.3, 'I0': 1.2, 'T0': 0.5, 'toHospitalRate': 0.5}
+to_distort = {'T0': 0.5}
 
 M.addResult('det. hosp.', lambda State: tf.reduce_sum(Popul * State['hospitalized']))
 # simulate data
@@ -44,6 +50,7 @@ else:
     learnrate = {"nesterov": 1e-9, "adam": 7e-6}
     oparam = {"learning_rate": learnrate[otype]}
 oparam['noiseModel'] = 'Poisson'
+# oparam['noiseModel'] = 'ScaledGaussian'
 
 fittedVars, fittedRes = M.fit({'det. hosp.': measured}, Tmax, otype=otype, oparam=oparam, NIter=150, verbose=True, lossScale=lossScale)
 

Examples/FitCoronaExample.py
@@ -30,7 +30,7 @@ else:
 
 ReduceDistricts = True
 if ReduceDistricts:
-    DistrictStride = 50
+    DistrictStride = 200
     MeasDetected = MeasDetected[:, 0:-1:DistrictStride, :, :]
     PopM = PopM[0:-1:DistrictStride]
     PopW = PopW[0:-1:DistrictStride]
@@ -55,9 +55,15 @@ InitPopulW = M.Axes['District'].init(PopW / PopSum)
 InitPopul = InitPopulM + InitPopulW
 MRatio = np.sum(PopM) / PopSum
 M.newState(name='S', axesInit={"Age": InitAge, "District": InitPopul, "Gender": [MRatio, 1 - MRatio]})
-I0 = M.newVariables({'I0': 0.000055 * InitPopulM})  # a district dependent variable of initially infected
-InitProgression = lambda: I0() * M.Axes['Disease Progression'].initDelta()  # variables to fit have to always be packed in lambda functions!
-M.newState(name='I', axesInit={"Disease Progression": InitProgression, "District": None, "Age": None, "Gender": None})
+M.newState(name='Sq', axesInit={"Age": InitAge, "District": InitPopul, "Gender": [MRatio, 1 - MRatio]})
+#I0 = M.newVariables({'I0': 0.000055 * InitPopulM}, forcePos=False)  # a district dependent variable of initially infected
+I0 = M.newVariables({'I0': 0.01 * InitPopulM}, forcePos=False)  # a district dependent variable of initially infected
+# InitProgression = lambda: I0 * M.Axes['Disease Progression'].initDelta()  # variables to fit have to always be packed in lambda functions!
+#M.newState(name='I', axesInit={"Disease Progression": InitProgression, "District": None, "Age": None, "Gender": None})
+M.newState(name='I', axesInit={"Disease Progression": 0, "District": 0, "Age": 0, "Gender": 0})
+T0 = M.newVariables({"T0": 64.5}, forcePos=False)  # time at which a delta is injected into the progression
+M.addRate('S', 'I', lambda t: I0 * M.initGaussianT0(T0, t), queueDst='Disease Progression', hasTime=True)  # When you made it though the queue, you are recovered
+
 M.newState(name='H', axesInit={"Disease Progression": 0, "District": 0, "Age": 0, "Gender": 0})
 M.newState(name='R', axesInit={"District": 0, "Age": 0, "Gender": 0})  # undetected recovered
 M.newState(name='Rd', axesInit={"District": 0, "Age": 0, "Gender": 0})  # detected recovered
@@ -66,15 +72,17 @@ hr = M.newVariables({'hr': 0.02})  # rate of hospitalization
 hospitalization = lambda: hr() * M.Axes['Disease Progression'].initGaussian(ht0, 3.0)
 influx = M.newVariables({'influx': 0.0001})  # a district dependent variable of initially infected
 # infectionRate = lambda I: (I + influx) * M.Var['r0']
-r0 = M.newVariables({'r0': 0.11 / InitPopul})
+r0 = M.newVariables({'r0': 0.11 / InitPopul}, forcePos=True)
 M.addRate(('S', 'I'), 'I', r0, queueDst="Disease Progression")  # S ==> I[0]
 M.addRate('I', 'H', hospitalization)  # I[t] -> H[t]
-M.addRate('H', 'Rd', 1.0, queueSrc="Disease Progression")  # H[t] -> R[t]  this is a dequeuing operation and thus the rate needs to be one!
-M.addRate('I', 'R', 1.0, queueSrc="Disease Progression")  # H[t] -> R[t]  this is a dequeuing operation and thus the rate needs to be one!
+M.addRate('H', 'Rd', 1.0, queueSrc="Disease Progression")  # H[t] -> R[t]  this is a dequeueing operation and thus the rate needs to be one!
+M.addRate('I', 'R', 1.0, queueSrc="Disease Progression")  # H[t] -> R[t]  this is a dequeueing operation and thus the rate needs to be one!
 M.addResult('detected', lambda State: tf.reduce_sum(State['H'], 1) + State['Rd'])  # ('I', 'S')
 
 # M.toFit(['r0', 'hr', 'ht0', 'I0'])
-M.toFit(['r0', 'I0'])
+# M.toFit(['r0', 'I0'])
+M.toFit(['T0', 'r0'])
+# M.toFit(['r0'])
 
 # simulated = M.simulate('simulated', {'detected': None}, Tmax=Tmax)
 # M.showResults(ylabel='occupancy')